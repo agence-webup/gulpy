702ff2b13774342c0e2247545a3dfebcce529021
Robin PARISI <parisi.robin@gmail.com>
2019-08-08 17:03:47 +0200

    Fix copyNpm() excluded files and folders

--- src/tasks/CopyNpm.js
+++ src/tasks/CopyNpm.js
@@ -1,13 +1,44 @@
 const gulp = require('gulp')
 const npmDist = require('gulp-npm-dist')
 
+const EXCLUDED_FROM_NPM = [
+  '*.map',
+  'examples/**/*',
+  'example/**/*',
+  'demo/**/*',
+  'spec/**/*',
+  'docs/**/*',
+  'tests/**/*',
+  'test/**/*',
+  'Gruntfile.js',
+  'gulpfile.js',
+  'package.json',
+  'package-lock.json',
+  'bower.json',
+  'composer.json',
+  'yarn.lock',
+  'webpack.config.js',
+  'README',
+  'LICENSE',
+  'CHANGELOG',
+  '*.yml',
+  '*.md',
+  '*.coffee',
+  '*.ts',
+  '*.scss',
+  '*.less'
+]
+
 module.exports = class CopyNpm {
   constructor (options) {
     this.options = options
   }
 
   getTask (dist) {
-    const packages = npmDist()
+    const packages = npmDist({
+      copyUnminified: true,
+      excludes: EXCLUDED_FROM_NPM
+    })
     // if there is no packages to copy
     if (packages.length === 0) {
       return function copyNpm () {
