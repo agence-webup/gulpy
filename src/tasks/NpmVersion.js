44d92d9a61231e73c1a138f055db5d99ec1f17a5
Robin PARISI <parisi.robin@gmail.com>
2019-08-26 20:22:13 +0200

    Enhance npmVersion()

--- src/tasks/NpmVersion.js
+++ src/tasks/NpmVersion.js
@@ -10,14 +10,15 @@ module.exports = class NpmVersion {
     const self = this
     return function npmVersion (cb) {
       try {
-        const data = fs.readFileSync('package-lock.json')
+        console.log(process.cwd())
+        const data = fs.readFileSync('./package-lock.json')
         const dependencies = JSON.parse(data).dependencies
         const versions = {}
         Object.entries(dependencies).forEach((el) => {
           const cacheValue = el[1].version ? el[1].version : 'noversion'
           versions[el[0]] = crypto.createHash('md5').update(cacheValue).digest('hex')
         })
-        fs.writeFileSync(self.options.npmManifest, JSON.stringify(versions))
+        fs.writeFileSync(self.options.npmManifest, JSON.stringify(versions, null, 4))
         cb()
       } catch (error) {
         cb(new Error('Unable to read package-lock.json'))
