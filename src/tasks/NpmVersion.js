b79b201a72ef36ed77cd87ef3be9875f28b06075
Robin Parisi <parisi.robin@gmail.com>
2025-07-01 17:36:41 +0200

    Fix manifest by using new key and remove root package

--- src/tasks/NpmVersion.js
+++ src/tasks/NpmVersion.js
@@ -11,14 +11,17 @@ module.exports = class NpmVersion {
     return function npmVersion(cb) {
       try {
         const data = fs.readFileSync('./package-lock.json')
-        const dependencies = JSON.parse(data).dependencies
+        const dependencies = JSON.parse(data).packages
         const versions = {}
         Object.entries(dependencies).forEach((el) => {
-          const cacheValue = el[1].version ? el[1].version : 'noversion'
-          versions[el[0]] = crypto
-            .createHash('md5')
-            .update(cacheValue)
-            .digest('hex')
+          // Skip the root package (empty key) and only process actual dependencies
+          if (el[0] && el[0] !== '') {
+            const cacheValue = el[1].version ? el[1].version : 'noversion'
+            versions[el[0]] = crypto
+              .createHash('md5')
+              .update(cacheValue)
+              .digest('hex')
+          }
         })
         fs.writeFileSync(
           self.options.npmManifest,
